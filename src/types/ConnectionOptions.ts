eb3c0e471dca7c56ee69a3e78eb7d6b0ef8e4103
Shivananda Sahu <sshivananda@athenahealth.com>
2019-11-03 17:50:17 +0530

    Fixing formatting in comments

--- src/types/ConnectionOptions.ts
+++ src/types/ConnectionOptions.ts
@@ -27,7 +27,8 @@ export interface ConnectionOptions {
   warehouse?: string;
   /** The default security role to use for the session after connecting. */
   role?: string;
-  /** By default, client connections typically time out approximately 3-4 hours
+  /**
+   * By default, client connections typically time out approximately 3-4 hours
    * after the most recent query was executed. If the parameter clientSessionKeepAlive is set to true,
    * the client’s connection to the server will be kept alive indefinitely, even if no queries are executed.
    * The default setting of this parameter is false. If you set this parameter to true, make sure that your
